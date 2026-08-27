caefabecfad23beaf18b73d137872688c0d26391
Suuu <yisshiki39@gmail.com>
2026-08-27 23:36:20 +0900

    chore: add Cloudflare frontend deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
-export default defineConfig([...nextVitals, globalIgnores([".next/**"])]);
-
+export default defineConfig([...nextVitals, globalIgnores([".next/**", "dist/**", ".vinext/**", ".wrangler/**"])]);
